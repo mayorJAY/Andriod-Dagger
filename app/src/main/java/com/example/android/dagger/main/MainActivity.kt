73b05ca201ae991f48883886defadce62b2b334f
mayorjay <joseolu4gsm@yahoo.com>
2021-06-15 00:56:09 +0100

    feat: Multiple activities with the same scope

--- app/src/main/java/com/example/android/dagger/main/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -34,16 +34,13 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var mainViewModel: MainViewModel
 
-    @Inject
-    lateinit var userManager: UserManager
-
     /**
      * If the User is not registered, RegistrationActivity will be launched,
      * If the User is not logged in, LoginActivity will be launched,
      * else carry on with MainActivity
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        (application as MyApplication).appComponent.inject(this)
+        val userManager = (application as MyApplication).appComponent.userManager()
         super.onCreate(savedInstanceState)
 
         if (!userManager.isUserLoggedIn()) {
@@ -56,6 +53,7 @@ class MainActivity : AppCompatActivity() {
             }
         } else {
             setContentView(R.layout.activity_main)
+            userManager.userComponent!!.inject(this)
 
             setupViews()
         }
